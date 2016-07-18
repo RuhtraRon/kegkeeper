a244549e7db893b8983f152f61b9966a1865586d
RuhtraRon <rpulliam@gmail.com>
2016-07-18 15:36:44 -0400

    added OTA

--- FlowMeter/ValveControl.ino
+++ FlowMeter/ValveControl.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
-//#include <EEPROM.h>
-//#include <DNSServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 #include "WiFiManager.h"
 #include <Ticker.h>
@@ -53,6 +54,30 @@ void flashLED(int Qty,int Dur){
   }
 }
 
+void ArduinoOTAsetup(){
+  ArduinoOTA.onStart([]() {
+  Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() 
 {
   initHardware();
@@ -66,6 +91,7 @@ void setup()
 
   server.begin();
   flashLED(3,100);
+  ArduinoOTAsetup();
 }
 
 void loop() 
@@ -73,6 +99,7 @@ void loop()
   //Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
+    ArduinoOTA.handle();
     return;
   }
 
